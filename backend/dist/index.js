2f321be19e57157fbb86afef420f9b46ed7a09fb
Yash Mahto <yashmahto244@gmail.com>
2026-04-27 00:01:09 +0530

    connect postgresql and create schema

---
+++ backend/dist/index.js
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+dotenv.config({
+    path: "./.env",
+});
+import { app } from './app.js';
+const PORT = process.env.PORT || 4001;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
